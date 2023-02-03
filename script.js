1a86c30c075ab8440e9372273ca1418267d41068
Anna Hrulckova <annahrulkova@yandex.ru>
2023-02-03 16:55:54 +0400

    ip

--- script.js
+++ script.js
@@ -1,11 +1,37 @@
 const btn = document.querySelector('button')
 const pictire = document.querySelector('.picture')
+
+const p = document.createElement("p")
+const wrap = document.querySelector('.wrap').appendChild(p)
+p.style.display="none"
+
+
+let h2 = document.querySelector("h2")
 btn.addEventListener('click', () => {
     if (!pictire.classList.contains('active')) {
         pictire.classList.add('active')
         pictire.style.display = 'block'
+        fetch('https://api.ipify.org?format=json')
+        .then(response => response.json())
+        .then(data => {
+          const ipAddress = data.ip;
+          p.style.display="block"
+           p.innerHTML=ipAddress
+        })
+        .catch(error => console.error(error));
+        
+
+
     } else {
         pictire.style.display = 'none'
         pictire.classList.remove('active')
+        location.reload()
+
+
+
+    
     }
 })
+
+
+
